20ff014e8315608d140fd843630a2bc27bed2bbd
bayertom <bayertom@natur.cuni.cz>
2024-12-10 00:15:52 +0100

    cv12

--- cv12/spat_hashing.py
+++ cv12/spat_hashing.py
@@ -19,23 +19,19 @@ def loadPoints(file):
     return X, Y, Z
 
 def  getNN(xq, yq, zq, X, Y, Z):
-    #Find nearest point
+    #Find nearest point and its distance
     dmin = inf
     xn, yn, zn = X[0], Y[0], Z[0]
     
     for i in range(len(X)):
         #Compute distance
-        dx = xq - X[i]
-        dy = yq - Y[i]
-        dz = zq - Z[i]
+        dx, dy, dz = xq - X[i], yq - Y[i], zq - Z[i]
         d = (dx*dx + dy*dy + dz * dz)**0.5
         
-        #Actualize minimum
+        #Actualize minimum: distance + coordinates
         if d < dmin:
             dmin = d
-            xn = X[i]
-            yn = Y[i]
-            zn = Z[i]
+            xn, yn, zn = X[i], Y[i], Z[i]
     return xn, yn, zn, dmin
 
 def drawPoints(X, Y, Z, bx):
@@ -51,5 +47,5 @@ def drawPoints(X, Y, Z, bx):
         
     #Plot points
     ax.scatter(X, Y, Z, s=scale, alpha = 0.2)
-    
+
     show()
